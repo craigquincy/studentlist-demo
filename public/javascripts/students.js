0913efba5cbf4bf3a1ea4a76cabc8a8438196e98
Craig Quincy <craig.quincy@galvanize.com>
2017-10-05 14:04:50 -0600

    student list via ajax

--- public/javascripts/students.js
+++ public/javascripts/students.js
@@ -1,3 +1,15 @@
 $(document).ready(() => {
-  console.log("is this js working?");
+
+  $.getJSON('/api/v1/students').then(data => {
+    console.log("get students", data)
+    let tbody = $('#studentList tbody')
+
+    data.forEach((student, i) => {
+      tbody.append($(`<tr>
+        <td>${student.id}</td>
+        <td>${student.name}</td>
+        <td>${student.mantra}</td>
+      </tr>`))
+    })
+  })
 })
