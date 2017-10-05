eb2d129c603a8aadaa6911ad85d1b1cd62cab190
Craig Quincy <craig.quincy@galvanize.com>
2017-10-05 13:41:04 -0600

    basic layout working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', _layoutFile: 'layout.ejs' });
+  res.render('index', { title: 'Is this page working?', _layoutFile: 'layout.ejs' });
 });
 
 module.exports = router;
